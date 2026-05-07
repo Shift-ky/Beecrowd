dc0d31ac3cc8690a8f3e53764a2a9711417e75b1
Robson Paiva <103571578+Shift-ky@users.noreply.github.com>
2026-05-06 21:54:10 -0300

    atualização

--- problema 1013.py
+++ problema 1013.py
@@ -1,7 +1,8 @@
+numero = list(map(int, input().split()))
 
-
-
-
-
-
-
+if numero[0] > numero[1] and numero[0] > numero[2]:
+    print(f'{numero[0]} eh maior')
+elif numero[2] > numero[0] and numero[2] > numero[1]:
+    print(f'{numero[2]} eh maior')
+else:
+    print(f'{numero[1]} eh maior')
